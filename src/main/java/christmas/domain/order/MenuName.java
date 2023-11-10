217dbeb2563f38cdaeeaada7a4a0147b83399aab
seokmyungham <hellotjraud@gmail.com>
2023-11-10 20:08:23 +0900

    feat(MenuName): validateMenuName 메서드 추가 및 객체 생성시 검증하도록 변경
    validateMenuName: Enum 클래스 Menu 메서드를 호출해서 입력으로 들어온 menuName이 존재하는 메뉴 이름인지 검증합니다

--- src/main/java/christmas/domain/order/MenuName.java
+++ src/main/java/christmas/domain/order/MenuName.java
@@ -1,10 +1,18 @@
 package christmas.domain.order;
 
+import christmas.domain.Menu;
+
 public class MenuName {
-    private String menuName;
+    private final String menuName;
 
     public MenuName(String menuName) {
+        validateMenuName(menuName);
         this.menuName = menuName;
     }
 
+    private void validateMenuName(String menuName) {
+        if (!Menu.containsName(menuName)) {
+            throw new IllegalArgumentException("메뉴판에 없는 메뉴 이름입니다.");
+        }
+    }
 }
