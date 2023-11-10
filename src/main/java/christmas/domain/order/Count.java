dec07f45a14ed72d1487b5abbf17d36d1a173788
seokmyungham <hellotjraud@gmail.com>
2023-11-10 20:16:49 +0900

    feat(Count): validateCount 메서드 추가 및 객체 생성시 검증
    validateCount: 매개 변수 count가 1 이상인지 검증합니다

--- src/main/java/christmas/domain/order/Count.java
+++ src/main/java/christmas/domain/order/Count.java
@@ -1,9 +1,18 @@
 package christmas.domain.order;
 
+import static christmas.constants.ErrorMessage.INVALID_ORDER_ERROR_MESSAGE;
+
 public class Count {
     private final int count;
 
     public Count(int count) {
+        validateCount(count);
         this.count = count;
+    }
+
+    private void validateCount(int count) {
+        if (count < 1) {
+            throw new IllegalArgumentException(INVALID_ORDER_ERROR_MESSAGE);
+        }
     }
 }
