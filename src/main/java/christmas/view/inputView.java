df7f2b6c5f2cd058f9a5a06c009ee5643f364a03
seokmyungham <hellotjraud@gmail.com>
2023-11-10 15:33:01 +0900

    feat(inputView): inputVisitDate, inputOrderMenu 메서드 추가
    inputVisitDate: 웰컴 메시지와 프롬프트 메시지를 출력하고 식당 예상 방문 날짜를 입력 받습니다
    inputOrderMenu: 프롬프트 메시지를 출력하고 주문 메뉴와 메뉴의 개수를 입력 받습니다

--- src/main/java/christmas/view/inputView.java
+++ src/main/java/christmas/view/inputView.java
@@ -1,7 +1,20 @@
 package christmas.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class inputView {
     private static final String WELCOME_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String VISIT_DATE_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     private static final String ORDER_MENU_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물 파스타-2,레드와인-1,초코케이크-1)";
+
+    public String inputVisitDate() {
+        System.out.println(WELCOME_MESSAGE);
+        System.out.println(VISIT_DATE_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String inputOrderMenu() {
+        System.out.println(ORDER_MENU_MESSAGE);
+        return Console.readLine();
+    }
 }
