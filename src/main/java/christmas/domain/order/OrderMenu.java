c4a4f76339c8216a05179d6cb1f98b0137e3671a
seokmyungham <hellotjraud@gmail.com>
2023-11-12 15:54:17 +0900

    feat(OrderMenu): getOrderMenuPrice 메서드 추가
    getOrderMenuPrice: 주문한 메뉴 이름과 수량으로, 한 메뉴의 주문 가격을 계산합니다

--- src/main/java/christmas/domain/order/OrderMenu.java
+++ src/main/java/christmas/domain/order/OrderMenu.java
@@ -1,5 +1,7 @@
 package christmas.domain.order;
 
+import christmas.domain.Menu;
+
 public class OrderMenu {
     private final MenuName menuName;
     private final Count count;
@@ -7,5 +9,12 @@ public class OrderMenu {
     public OrderMenu(MenuName menuName, Count count) {
         this.menuName = menuName;
         this.count = count;
+    }
+
+    public int getOrderMenuPrice() {
+        int price = Menu.getPriceByName(menuName.getMenuName());
+        int count = this.count.getCount();
+
+        return price * count;
     }
 }
