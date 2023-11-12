c0a2f5152e0428d7a5a3b47b722a091e433b0d5f
seokmyungham <hellotjraud@gmail.com>
2023-11-13 05:14:46 +0900

    test(WeekdayDiscountTest): applyDiscount 메서드 테스트 추가

---
+++ src/test/java/christmas/domain/event/discount/WeekdayDiscountTest.java
@@ -0,0 +1,35 @@
+package christmas.domain.event.discount;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.domain.order.Count;
+import christmas.domain.order.MenuName;
+import christmas.domain.order.Order;
+import christmas.domain.order.OrderMenu;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class WeekdayDiscountTest {
+    private Order totalOrder;
+
+    @BeforeEach
+    void setUp() {
+        totalOrder = new Order(List.of(
+                new OrderMenu(new MenuName("타파스"), new Count(1)),
+                new OrderMenu(new MenuName("시저샐러드"), new Count(1)),
+                new OrderMenu(new MenuName("크리스마스파스타"), new Count(2)),
+                new OrderMenu(new MenuName("초코케이크"), new Count(1)),
+                new OrderMenu(new MenuName("레드와인"), new Count(1))));
+    }
+
+
+    @Test
+    void applyDiscountTest() {
+        WeekdayDiscount weekdayDiscount = new WeekdayDiscount();
+
+        int discountPrice = weekdayDiscount.applyDiscount(totalOrder);
+
+        assertThat(discountPrice).isEqualTo(2023);
+    }
+}
