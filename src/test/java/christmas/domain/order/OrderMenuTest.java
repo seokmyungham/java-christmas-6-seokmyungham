1f7ad61527e6542cf3f005bd9974b1ee5b262068
seokmyungham <hellotjraud@gmail.com>
2023-11-13 03:56:17 +0900

    test(OrderMenuTest): 메서드 이름 변경에 따른 테스트 코드 개선

--- src/test/java/christmas/domain/order/OrderMenuTest.java
+++ src/test/java/christmas/domain/order/OrderMenuTest.java
@@ -9,18 +9,19 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class OrderMenuTest {
 
-    @DisplayName("메뉴 이름과 메뉴 개수로 OrderMenu를 생성해서 메뉴 주문 가격을 반환한다.")
+    @DisplayName("OrderMenu의 주문 가격 계산 테스트")
     @ParameterizedTest
     @CsvSource({
             "양송이수프, 2",
             "시저샐러드, 3",
             "해산물파스타, 4"
     })
-    void getOrderMenuPriceTest(String menu, int count) {
-        OrderMenu orderMenu = new OrderMenu(new MenuName(menu), new Count(count));
+    void getPriceTest(String menuName, int count) {
+        OrderMenu orderMenu = new OrderMenu(new MenuName(menuName), new Count(count));
+        int expectedPrice = Menu.getMenuByName(menuName).getPrice() * count;
 
-        int orderMenuPrice = orderMenu.getOrderMenuPrice();
+        int orderMenuPrice = orderMenu.getPrice();
 
-        assertThat(orderMenuPrice).isEqualTo(Menu.getPriceByName(menu) * count);
+        assertThat(orderMenuPrice).isEqualTo(expectedPrice);
     }
 }
