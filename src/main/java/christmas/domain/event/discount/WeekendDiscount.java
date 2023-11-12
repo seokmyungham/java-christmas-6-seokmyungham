15f500dd015052d01fdb016bc4bf8049e4bd4cd9
seokmyungham <hellotjraud@gmail.com>
2023-11-13 05:11:06 +0900

    feat(WeekendDiscount): 클래스 생성 및 applyDiscount 메서드 오버라이드
    applyDiscount: 주문한 특정 메뉴 유형 1개당 할인을 적용하는 메서드입니다

---
+++ src/main/java/christmas/domain/event/discount/WeekendDiscount.java
@@ -0,0 +1,13 @@
+package christmas.domain.event.discount;
+
+import christmas.domain.MenuType;
+import christmas.domain.order.Order;
+
+public class WeekendDiscount implements Discount {
+    private static final int discountPrice = 2023;
+
+    @Override
+    public int applyDiscount(Order order) {
+        return order.countMenuType(MenuType.MAIN) * discountPrice;
+    }
+}
