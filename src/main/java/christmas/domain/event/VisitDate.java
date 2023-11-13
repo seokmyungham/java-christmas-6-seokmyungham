a3148d58390783265f332ae73e7ff780df2337de
seokmyungham <hellotjraud@gmail.com>
2023-11-13 17:29:43 +0900

    feat(VisitDate): 상수 추가, daysElapsed 메서드 추가
    daysElapsed: 이벤트 시작 날짜부터 방문 날짜까지 경과된 일 수를 계산합니다

--- src/main/java/christmas/domain/event/VisitDate.java
+++ src/main/java/christmas/domain/event/VisitDate.java
@@ -3,11 +3,17 @@ package christmas.domain.event;
 import static christmas.constants.ErrorMessage.INVALID_DATE_ERROR_MESSAGE;
 
 public class VisitDate {
+    private static final int START_DATE = 1;
+
     private final int visitDate;
 
     public VisitDate(int visitDate) {
         validateVisitDate(visitDate);
         this.visitDate = visitDate;
+    }
+
+    public int daysElapsed() {
+        return visitDate - START_DATE;
     }
 
     private void validateVisitDate(int visitDate) {
