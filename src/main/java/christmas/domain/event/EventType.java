d4d19a214019ce45b7a55f6844cd49d59a9eab3f
seokmyungham <hellotjraud@gmail.com>
2023-11-13 20:41:06 +0900

    feat(EventType): enum 클래스 생성

---
+++ src/main/java/christmas/domain/event/EventType.java
@@ -0,0 +1,15 @@
+package christmas.domain.event;
+
+public enum EventType {
+    CHRISTMAS_D_DAY_DISCOUNT("크리스마스 디데이 할인"),
+    WEEKDAY_DISCOUNT("평일 할인"),
+    WEEKEND_DISCOUNT("주말 할인"),
+    STAR_DAY_DISCOUNT("특별 할인"),
+    GIFT_EVENT("증정 이벤트");
+
+    private final String name;
+
+    EventType(String name) {
+        this.name = name;
+    }
+}
