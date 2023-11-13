c406cd507b2a25d11ca198b548aacea83a0e9d93
seokmyungham <hellotjraud@gmail.com>
2023-11-14 00:07:11 +0900

    refactor(GiftEvent): Event 인터페이스 상속 및 코드 리팩토링
    책임 원칙을 고려하여 이벤트 적용 여부를 클래스 내에서 판단하도록 코드를 개선하고, 이벤트 타입을 반환하는 메서드를 추가하였습니다

--- src/main/java/christmas/domain/event/GiftEvent.java
+++ src/main/java/christmas/domain/event/GiftEvent.java
@@ -1,13 +1,24 @@
 package christmas.domain.event;
 
 import static christmas.domain.Menu.CHAMPAGNE;
+import static christmas.domain.event.EventType.GIFT_EVENT;
 
 import christmas.domain.Menu;
+import christmas.domain.order.Order;
 
-public class GiftEvent {
+public class GiftEvent implements Event{
     private static final Menu GIFT = CHAMPAGNE;
 
-    public int apply() {
-        return GIFT.getPrice();
+    @Override
+    public int apply(Order order, VisitDate visitDate) {
+        if (order.totalPrice() >= 120000) {
+            return GIFT.getPrice();
+        }
+        return 0;
+    }
+
+    @Override
+    public EventType getType() {
+        return GIFT_EVENT;
     }
 }
