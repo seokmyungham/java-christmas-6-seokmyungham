2747a6ff17e178a52aee445141bc5af6ac791218
seokmyungham <hellotjraud@gmail.com>
2023-11-14 00:02:08 +0900

    refactor(StarDayDiscount): Event 인터페이스 상속 및 코드 리팩토링
    책임 원칙을 고려하여 이벤트 적용 여부를 클래스 내에서 판단하도록 코드를 개선하고, 이벤트 타입을 반환하는 메서드를 추가하였습니다

--- src/main/java/christmas/domain/event/discount/StarDayDiscount.java
+++ src/main/java/christmas/domain/event/discount/StarDayDiscount.java
@@ -1,9 +1,28 @@
 package christmas.domain.event.discount;
 
-public class StarDayDiscount {
-    private static final int discountPrice = 1000;
+import static christmas.domain.event.EventType.STAR_DAY_DISCOUNT;
 
-    public int apply() {
-        return discountPrice;
+import christmas.domain.event.Event;
+import christmas.domain.event.EventType;
+import christmas.domain.event.VisitDate;
+import christmas.domain.order.Order;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StarDayDiscount implements Event {
+    private static final int discountPrice = 1000;
+    private static final List<Integer> STAR_DAY = new ArrayList<>(List.of(3, 10, 17, 24, 25, 31));
+
+    @Override
+    public int apply(Order order, VisitDate visitDate) {
+        if (visitDate.isVisitInEventDay(STAR_DAY)) {
+            return discountPrice;
+        }
+        return 0;
+    }
+
+    @Override
+    public EventType getType() {
+        return STAR_DAY_DISCOUNT;
     }
 }
