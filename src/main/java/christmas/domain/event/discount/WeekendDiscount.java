929fc0cee349b529a2b1cea6581401d0cfdde6db
seokmyungham <hellotjraud@gmail.com>
2023-11-14 00:04:26 +0900

    refactor(WeekendDiscount): 코드 리팩토링
    책임 원칙을 고려하여 이벤트 적용 여부를 클래스 내에서 판단하도록 코드를 개선하고, 이벤트 타입을 반환하는 메서드를 추가하였습니다

--- src/main/java/christmas/domain/event/discount/WeekendDiscount.java
+++ src/main/java/christmas/domain/event/discount/WeekendDiscount.java
@@ -1,13 +1,27 @@
 package christmas.domain.event.discount;
 
+import static christmas.domain.event.EventType.WEEKEND_DISCOUNT;
+
 import christmas.domain.MenuType;
+import christmas.domain.event.Event;
+import christmas.domain.event.EventType;
+import christmas.domain.event.VisitDate;
 import christmas.domain.order.Order;
 
-public class WeekendDiscount implements Discount {
+public class WeekendDiscount implements Event {
+    private static final int YEAR = 2023, MONTH = 12;
     private static final int discountPrice = 2023;
 
     @Override
-    public int apply(Order order) {
-        return order.countMenuType(MenuType.MAIN) * discountPrice;
+    public int apply(Order order, VisitDate visitDate) {
+        if (visitDate.isWeekday(YEAR, MONTH)) {
+            return order.countMenuType(MenuType.MAIN) * discountPrice;
+        }
+        return 0;
+    }
+
+    @Override
+    public EventType getType() {
+        return WEEKEND_DISCOUNT;
     }
 }
