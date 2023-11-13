8ee0c965da235d8ab673f975958e60dffc88f194
seokmyungham <hellotjraud@gmail.com>
2023-11-13 17:33:44 +0900

    test(VisitDateTest): daysElapsed 메서드 테스트 추가

--- src/test/java/christmas/domain/event/VisitDateTest.java
+++ src/test/java/christmas/domain/event/VisitDateTest.java
@@ -1,11 +1,13 @@
 package christmas.domain.event;
 
 import static christmas.constants.ErrorMessage.INVALID_DATE_ERROR_MESSAGE;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class VisitDateTest {
@@ -25,5 +27,15 @@ class VisitDateTest {
     void validVisitDateNoExceptionTest(int visitDate) {
         assertThatNoException()
                 .isThrownBy(() -> new VisitDate(visitDate));
+    }
+
+    @DisplayName("방문 날짜가 이벤트 시작 날짜로부터 몇 일이 경과되었는지 계산한다.")
+    @ParameterizedTest
+    @CsvSource({
+            "1, 0",
+            "10, 9",
+    })
+    void daysElapsedTest(int visitDate, int startDate) {
+        assertThat(new VisitDate(visitDate).daysElapsed()).isEqualTo(startDate);
     }
 }
