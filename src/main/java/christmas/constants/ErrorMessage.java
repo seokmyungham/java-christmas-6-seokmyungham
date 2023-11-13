67095807d8d31aca2daf1f21f848f3ca8dab72d8
seokmyungham <hellotjraud@gmail.com>
2023-11-13 14:43:29 +0900

    feat(ErrorMessage): 유효하지 않은 방문 날짜 관련 상수 추가

--- src/main/java/christmas/constants/ErrorMessage.java
+++ src/main/java/christmas/constants/ErrorMessage.java
@@ -2,4 +2,5 @@ package christmas.constants;
 
 public class ErrorMessage {
     public static final String INVALID_ORDER_ERROR_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    public static final String INVALID_DATE_ERROR_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
 }
