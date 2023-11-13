7f3d87b675db4a747cc5517ebae2cf18c48b0d9f
seokmyungham <hellotjraud@gmail.com>
2023-11-13 17:35:43 +0900

    test(ChristmasDiscountTest): apply 메서드 테스트 추가

---
+++ src/test/java/christmas/domain/event/discount/ChristmasDiscountTest.java
@@ -0,0 +1,23 @@
+package christmas.domain.event.discount;
+
+import christmas.domain.event.VisitDate;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class ChristmasDiscountTest {
+
+    @DisplayName("이벤트 시작 일부터 방문 날짜까지 하루에 100원씩 할인 금액이 증가한다.")
+    @ParameterizedTest
+    @CsvSource({
+            "1, 1000",
+            "10, 1900",
+            "25, 3400"
+    })
+    void applyTest(int visitDate, int discountPrice) {
+        ChristmasDiscount christmasDiscount = new ChristmasDiscount();
+
+        Assertions.assertThat(christmasDiscount.apply(new VisitDate(visitDate))).isEqualTo(discountPrice);
+    }
+}
