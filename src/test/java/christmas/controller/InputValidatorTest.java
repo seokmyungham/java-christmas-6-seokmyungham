79d5ddfaebe7479ac2866447a4ab117ee91b9785
seokmyungham <hellotjraud@gmail.com>
2023-11-14 11:38:05 +0900

    test(InputValidatorTest): validateInputFormat 메서드 테스트 추가

--- src/test/java/christmas/controller/InputValidatorTest.java
+++ src/test/java/christmas/controller/InputValidatorTest.java
@@ -1,6 +1,7 @@
 package christmas.controller;
 
 import static christmas.constants.ErrorMessage.INVALID_DATE_ERROR_MESSAGE;
+import static christmas.constants.ErrorMessage.INVALID_ORDER_ERROR_MESSAGE;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -26,6 +27,22 @@ class InputValidatorTest {
     void inputIsNumericNoExceptionTest(String input) {
         assertThatNoException()
                 .isThrownBy(() -> new InputValidator().validateIsNumeric(input));
+    }
 
+    @DisplayName("주문 입력이 형식과 다를 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"양송이수프2", "제로콜라_2"})
+    void invalidInputFormatExceptionTest(String input) {
+        assertThatThrownBy(() -> new InputValidator().validateInputFormat(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_ORDER_ERROR_MESSAGE);
+    }
+
+    @DisplayName("주문 입력이 형식과 같을 경우 예외가 발생하지 않는다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"양송이수프-2", "제로콜라-2"})
+    void validInputFormatNoExceptionTest(String input) {
+        assertThatNoException()
+                .isThrownBy(() -> new InputValidator().validateInputFormat(input));
     }
 }
