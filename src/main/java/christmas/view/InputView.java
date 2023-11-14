e6ccf1890cf35bab52b30a7c9241889b73d0d33e
seokmyungham <hellotjraud@gmail.com>
2023-11-15 03:06:21 +0900

    fix(InputView): 명령 프롬프트 메시지 띄어쓰기 수정

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private static final String WELCOME_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String VISIT_DATE_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
-    private static final String ORDER_MENU_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물 파스타-2,레드와인-1,초코케이크-1)";
+    private static final String ORDER_MENU_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
     public String inputVisitDate() {
         System.out.println(WELCOME_MESSAGE);
