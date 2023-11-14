664893e2913d03b5d7c28ecd504ed8e0e400e073
seokmyungham <hellotjraud@gmail.com>
2023-11-14 20:06:59 +0900

    feat(GiftEvent): getGift 메서드 추가 및 코드 리팩토링
    getGift: 증정 메뉴와 개수를 map에 담아 반환합니다
    meetRequirements: 기존의 이벤트 조건 로직을 메서드로 분리하였습니다

--- src/main/java/christmas/domain/event/GiftEvent.java
+++ src/main/java/christmas/domain/event/GiftEvent.java
@@ -5,14 +5,17 @@ import static christmas.domain.event.EventType.GIFT_EVENT;
 
 import christmas.domain.Menu;
 import christmas.domain.order.Order;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GiftEvent implements Event{
     private static final Menu GIFT = CHAMPAGNE;
+    private static final int count = 1;
 
     @Override
     public int apply(Order order, VisitDate visitDate) {
-        if (order.totalPrice() >= 120000) {
-            return GIFT.getPrice();
+        if (meetRequirements(order)) {
+            return GIFT.getPrice() * count;
         }
         return 0;
     }
@@ -20,5 +23,17 @@ public class GiftEvent implements Event{
     @Override
     public EventType getType() {
         return GIFT_EVENT;
+    }
+
+    public boolean meetRequirements(Order order) {
+        return order.totalPrice() >= 120000;
+    }
+
+    public Map<Menu, Integer> getGift(Order order) {
+        Map<Menu, Integer> giftInfo = new HashMap<>();
+        if (meetRequirements(order)) {
+            giftInfo.put(GIFT, count);
+        }
+        return giftInfo;
     }
 }
