e365f2c1f4d4ac7ceec11dca7840cfcfc7b6ebbe
seokmyungham <hellotjraud@gmail.com>
2023-11-14 18:20:59 +0900

    feat(EventController): getOrder 메서드 추가
    getOrder: 주문을 입력받아서 주문 형식과 같은지 검증하고 Order 객체를 생성해서 반환합니다
    getOrderMenus: 검증이 완료된 문자열을 콤마 단위로 파싱해서 OrderMenu 리스트에 추가하고 반환합니다
    getOrderMenu: 콤마 단위로 파싱된 문자열을 하이픈 단위로 파싱해서 OrderMenu 객체를 생성합니다

--- src/main/java/christmas/controller/EventController.java
+++ src/main/java/christmas/controller/EventController.java
@@ -1,8 +1,14 @@
 package christmas.controller;
 
 import christmas.domain.event.VisitDate;
+import christmas.domain.order.Count;
+import christmas.domain.order.MenuName;
+import christmas.domain.order.Order;
+import christmas.domain.order.OrderMenu;
 import christmas.service.EventService;
 import christmas.view.InputView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EventController {
     private final InputView inputView;
@@ -17,6 +23,7 @@ public class EventController {
 
     public void orderStart() {
         getVisitDate();
+        getOrder();
     }
 
     private VisitDate getVisitDate() {
@@ -30,4 +37,31 @@ public class EventController {
         }
     }
 
+    private Order getOrder() {
+        try {
+            String orderInput = inputView.inputOrderMenu();
+            inputValidator.validateInputFormat(orderInput);
+            return new Order(getOrderMenus(orderInput));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getOrder();
+        }
+    }
+
+    private List<OrderMenu> getOrderMenus(String orderInput) {
+        List<OrderMenu> orders = new ArrayList<>();
+
+        for (String parseByComma : InputParser.parseInputByComma(orderInput)) {
+            OrderMenu orderMenu = getOrderMenu(parseByComma);
+            orders.add(orderMenu);
+        }
+        return orders;
+    }
+
+    private OrderMenu getOrderMenu(String parseByComma) {
+        String menuName = InputParser.parseInputByHYPHEN(parseByComma).get(0);
+        String count = InputParser.parseInputByHYPHEN(parseByComma).get(1);
+
+        return new OrderMenu(new MenuName(menuName), new Count(Integer.parseInt(count)));
+    }
 }
