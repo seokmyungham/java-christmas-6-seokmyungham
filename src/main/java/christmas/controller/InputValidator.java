1156bf0b2b94721e1e16020e2647c664aa957ef7
seokmyungham <hellotjraud@gmail.com>
2023-11-14 11:37:45 +0900

    feat(InputValidator): validateInputFormat 메서드 추가
    validateInputFormat: 사용자 입력이 주문 형식을 지켰는지 검증합니다

--- src/main/java/christmas/controller/InputValidator.java
+++ src/main/java/christmas/controller/InputValidator.java
@@ -1,15 +1,23 @@
 package christmas.controller;
 
 import static christmas.constants.ErrorMessage.INVALID_DATE_ERROR_MESSAGE;
+import static christmas.constants.ErrorMessage.INVALID_ORDER_ERROR_MESSAGE;
 
 public class InputValidator {
     private static final String DELIMITER = ",";
+    private static final String ORDER_FORMAT = "^[가-힣]+-\\d+(,[가-힣]+-\\d+)*$";
 
     public void validateIsNumeric(String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(INVALID_DATE_ERROR_MESSAGE);
+        }
+    }
+
+    public void validateInputFormat(String input) {
+        if (!input.matches(ORDER_FORMAT)) {
+            throw new IllegalArgumentException(INVALID_ORDER_ERROR_MESSAGE);
         }
     }
 }
