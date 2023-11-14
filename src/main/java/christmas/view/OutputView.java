a7382c16fb0fb5437a54fbdf8e80206fd3fd71cc
seokmyungham <hellotjraud@gmail.com>
2023-11-14 20:11:04 +0900

    feat(OutputView): printGiftMenus 메서드 추가
    printGiftMenus: 증정 메뉴를 출력합니다

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,8 +1,10 @@
 package christmas.view;
 
+import christmas.domain.Menu;
 import christmas.domain.event.VisitDate;
 import christmas.domain.order.Order;
 import christmas.domain.order.OrderMenu;
+import java.util.Map;
 
 public class OutputView {
     private static final String PREVIEW_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!%n%n";
@@ -21,6 +23,16 @@ public class OutputView {
     }
 
     public void printPriceBeforeDiscount(Order order) {
-        System.out.printf("%n<할인 전 총주문 금액>%n%,d원", order.totalPrice());
+        System.out.printf("%n<할인 전 총주문 금액>%n%,d원%n", order.totalPrice());
+    }
+
+    public void printGiftMenus(Map<Menu, Integer> gifts) {
+        System.out.printf("%n<증정 메뉴>%n");
+        if (gifts.isEmpty()) {
+            System.out.printf("없음%n");
+        }
+        for (Menu gift : gifts.keySet()) {
+            System.out.printf("%s %d개%n", gift.getName(), gifts.get(gift));
+        }
     }
 }
