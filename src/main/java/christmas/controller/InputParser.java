98719e6738f5cb72d68c6c7fe1643979a06fe509
seokmyungham <hellotjraud@gmail.com>
2023-11-15 17:01:33 +0900

    refactor(InputParser): parseInputByComma, parseInputByHyphen 두 메서드를 하나로 리팩토링
    두 메서드를 하나로 통합하고 매개 변수로 구분자를 넘겨 받도록 변경하였습니다

--- src/main/java/christmas/controller/InputParser.java
+++ src/main/java/christmas/controller/InputParser.java
@@ -4,14 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InputParser {
-    private static final String DELIMITER_COMMA = ",";
-    private static final String DELIMITER_HYPHEN = "-";
-
-    public static List<String> parseInputByComma(String input) {
-        return new ArrayList<>(List.of(input.split(DELIMITER_COMMA)));
-    }
-
-    public static List<String> parseInputByHYPHEN(String input) {
-        return new ArrayList<>(List.of(input.split(DELIMITER_HYPHEN)));
+    public static List<String> parseInputByDelimiter(String input, String DELIMITER) {
+        return new ArrayList<>(List.of(input.split(DELIMITER)));
     }
 }
