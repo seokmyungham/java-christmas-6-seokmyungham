f356de5b70b5671fe312563ee27fa9f82867f113
seokmyungham <hellotjraud@gmail.com>
2023-11-15 15:33:32 +0900

    refactor(Event): 인터페이스에 하위 클래스들이 공통으로 사용할 상수 추가

--- src/main/java/christmas/domain/event/Event.java
+++ src/main/java/christmas/domain/event/Event.java
@@ -4,6 +4,8 @@ import christmas.domain.VisitDate;
 import christmas.domain.order.Order;
 
 public interface Event {
+    int EVENT_REQUIREMENT = 10000;
+
     int apply(Order order, VisitDate visitDate);
 
     boolean meetRequirements(Order order);
