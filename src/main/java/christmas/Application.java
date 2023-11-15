6748631ea7e6e070696bd12198718465e662a12f
seokmyungham <hellotjraud@gmail.com>
2023-11-15 12:53:22 +0900

    feat(Application): controller 생성 및 호출

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,18 @@
 package christmas;
 
+import christmas.controller.EventController;
+import christmas.controller.OrderController;
+import christmas.domain.VisitDate;
+import christmas.domain.order.Order;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OrderController orderController = new OrderController();
+        EventController eventController = new EventController();
+
+        VisitDate visitDate = orderController.processGetVisitDate();
+        Order order = orderController.processGetOrder();
+
+        eventController.processEvents(order, visitDate);
     }
 }
