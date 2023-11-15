fb6434f5435de5aaa98149ebfad0340ebbbcd835
seokmyungham <hellotjraud@gmail.com>
2023-11-15 13:25:44 +0900

    test(OrderMenuTest): equals 메서드 테스트 추가

--- src/test/java/christmas/domain/order/OrderMenuTest.java
+++ src/test/java/christmas/domain/order/OrderMenuTest.java
@@ -4,11 +4,11 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import christmas.domain.Menu;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class OrderMenuTest {
-
     @DisplayName("OrderMenu의 주문 가격 계산 테스트")
     @ParameterizedTest
     @CsvSource({
@@ -23,5 +23,30 @@ class OrderMenuTest {
         int orderMenuPrice = orderMenu.calculatePrice();
 
         assertThat(orderMenuPrice).isEqualTo(expectedPrice);
+    }
+
+    @DisplayName("equals 메서드 오버라이드 -> 동일한 객체를 비교할 시 참을 반환합니다")
+    @Test
+    void equalsObjTest() {
+        OrderMenu orderMenu = new OrderMenu(new MenuName("양송이수프"), new Count(1));
+
+        assertThat(orderMenu).isEqualTo(orderMenu);
+    }
+
+    @DisplayName("equals 메서드 오버라이드 -> 비교 대상이 OrderMenu의 인스턴스가 아니면 거짓을 반환합니다.")
+    @Test
+    void equalsNotInstanceofObjTest() {
+        OrderMenu orderMenu = new OrderMenu(new MenuName("양송이수프"), new Count(1));
+
+        assertThat(orderMenu.equals(new MenuName("양송이수프"))).isFalse();
+    }
+
+    @DisplayName("equals 메서드 오버라이드 -> 메뉴 이름이 같으면 같은 주문으로 판단합니다")
+    @Test
+    void equalsMenuNameTest() {
+        OrderMenu orderMenu = new OrderMenu(new MenuName("양송이수프"), new Count(1));
+        OrderMenu orderMenu2 = new OrderMenu(new MenuName("양송이수프"), new Count(2));
+
+        assertThat(orderMenu).isEqualTo(orderMenu2);
     }
 }
